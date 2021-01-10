5869ee9facaecbb5fbf7a4722c090e09247d4640
Erody <mrerody@googlemail.com>
2021-01-10 16:38:50 +0100

    add relationship between pizzas and toppings, add toppings as pizza subtitle

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -5,6 +5,26 @@ export default {
     title: 'Pizzas',
     type: 'document',
     icon,
+    preview: {
+        select: {
+            title: 'name',
+            media: 'image',
+            topping0: 'toppings.0.name',
+            topping1: 'toppings.1.name',
+            topping2: 'toppings.2.name',
+            topping3: 'toppings.3.name',
+        },
+        prepare: ({ title, media, ...toppings }) => {
+            // Filter undefined toppings out
+            const tops = Object.values(toppings).filter(Boolean);
+            // return the preview object for the pizza
+            return {
+                title,
+                media,
+                subtitle: tops.join(', '),
+            };
+        },
+    },
     fields: [
         {
             name: 'name',
@@ -35,6 +55,12 @@ export default {
             description: 'Price of the pizza in cents',
             validation: (Rule) => Rule.min(400).max(50000),
             // add custom input compononent
+        },
+        {
+            name: 'toppings',
+            title: 'Toppings',
+            type: 'array',
+            of: [{ type: 'reference', to: [{ type: 'topping' }] }],
         },
     ],
 };
