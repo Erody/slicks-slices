bb64ecc95e970860502d857aa9931f2ae53619bc
Immo <mrerody@googlemail.com>
2021-02-17 08:40:56 +0100

    style order page

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -5,6 +5,8 @@ import SEO from '../components/SEO';
 import useForm from '../utils/useForm';
 import calculatePizzaPrice from '../utils/calculatePizzaPrice';
 import formatMoney from '../utils/formatMoney';
+import Orderstyles from '../styles/OrderStyles';
+import MenuItemStyles from '../styles/MenuItemStyles';
 
 export default function OrderPage({ data }) {
     const { values, updateValue } = useForm({
@@ -15,7 +17,7 @@ export default function OrderPage({ data }) {
     return (
         <>
             <SEO title="Order a pizza" />
-            <form>
+            <Orderstyles>
                 <fieldset>
                     <legend>Your Info</legend>
                     <label htmlFor="name">
@@ -37,10 +39,10 @@ export default function OrderPage({ data }) {
                         />
                     </label>
                 </fieldset>
-                <fieldset>
+                <fieldset className="menu">
                     <legend>Menu</legend>
                     {pizzas.map((pizza) => (
-                        <div key={pizza.id}>
+                        <MenuItemStyles key={pizza.id}>
                             <Img
                                 width="50"
                                 height="50"
@@ -63,13 +65,13 @@ export default function OrderPage({ data }) {
                                     </button>
                                 ))}
                             </div>
-                        </div>
+                        </MenuItemStyles>
                     ))}
                 </fieldset>
-                <fieldset>
+                <fieldset className="order">
                     <legend>Order</legend>
                 </fieldset>
-            </form>
+            </Orderstyles>
         </>
     );
 }
